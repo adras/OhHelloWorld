7c6d8b673c6de7a97f5be71026e11c0ed1fbebfc
Andreas Kurka <andreas.kurka@gmail.com>
2022-05-20 18:48:58 +0200

    We're starting to get somewhere :D

--- EmojiWorld/Program.cs
+++ EmojiWorld/Program.cs
@@ -7,6 +7,23 @@ for (int i = 0; i < output.Length; i++)
     Console.WriteLine($"{i},{(int)output[i]}");
 }
 
+for (int i = 0; i < 10; i++, i+=2)
+{
+    Console.WriteLine("test");
+    Action<string> test = Console.WriteLine;
+    test("This is going to be cool");
+}
+
+// It's possible to define multiple variables, however they may not be a different type
+for (int a = 0, b; a < 10; a++) ;
+
+// This does not work
+// for (int a = 0, float b; a < 10; a++) ;
+
+// Oooh, fancy pancy dynamic does allow it
+// Cast is required though
+for (dynamic a = 0, b = (Action<string>)Console.WriteLine; 5 < 3; a++) ;
+
 // goal is something like this:
 for (string i = "💀☠🌞🐼😁🗿😍☝🥶🇵🇬🥳😎🚺✌⬅💦💪💖©💫💓🚯💣🇳🇮👽🤢😄👩🎯‍🏭🎁😬🧘‍♀️🤪🐨😚😇☠💀"; ;) ;
 
